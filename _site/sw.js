73581468ca6a29cabf11a41b9235f995db5564a5
Ta Duc Tung <tatung@Tas-MacBook-Pro-105.local>
2020-10-08 20:02:32 +0900

    Netlify post summary optional

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "saikiransripada-20201008194536";
+var CACHE_NAME = "saikiransripada-20201008200206";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/style.css?202010081945",
+        "/css/style.css?202010082002",
         "/"
       ]);
     })
