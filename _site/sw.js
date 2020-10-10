6e61e86767ac8e6b158af15474c9c26c0475245a
Ta Duc Tung <tatung@Tas-MacBook-Pro-105.local>
2020-10-11 01:23:22 +0900

    lowercase category link

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "saikiransripada-20201011005635";
+var CACHE_NAME = "saikiransripada-20201011012005";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/style.css?202010110056",
+        "/css/style.css?202010110120",
         "/"
       ]);
     })
